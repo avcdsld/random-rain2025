65351772caa9f6acf5ee0fd6bee93b0c9e3db492
avcdsld <avcdsld@gmail.com>
2025-11-14 12:15:13 +0900

    remove unused files

--- contracts/interfaces/IBefungeViewer.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
-
-interface IBefungeViewer {
-    function toSVG(string memory code) external view returns (string memory);
-    function toHTML(string memory code) external view returns (string memory);
-}
--- contracts/interfaces/IRandomRainGenerator.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
-
-interface IRandomRainGenerator {
-    function generateCode(uint256 seed) external view returns (string memory);
-}
